8ef26cc831782589256a28dc61ea7a7bfe88cc99
Cherrice Traver <traverc@union.edu>
2025-01-05 21:25:10 +0000

    More comments

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 //Another comment
 //Yet another comment - CT
 //And YET another comment - CT!
+//Change number 5
 #define NUMBER_OF_KEYS                           4
 #define BLINKING_TIME_GAS_ALARM               1000
 #define BLINKING_TIME_OVER_TEMP_ALARM          500
